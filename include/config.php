fac5b1e5b3bf598ace5bce34f49a9421e17fb755
yannngithub <yannngithub@gmail.com>
2022-12-19 19:29:40 +0700

    Mikhmon

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<alvianto','mikhmon>|>mZ2ooZKgrKA=');
+
+$data['Remote'] = array ('1'=>'Remote!remote.vpnmurahjogja.my.id:39202','Remote@|@admin','Remote#|#mZ2ooZKgrKBjb2FpamFiaw==','Remote%Alvianto Hotspot','Remote^alvianto.net.id','Remote&Rp','Remote*10','Remote(1','Remote)','Remote=10','Remote@!@disable');
